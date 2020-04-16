c23103a4d26ff70ebb2b765d3df240bd821f0a85
qianzhao <qianzhao@tencent.com>
2020-04-16 14:51:33 +0800

    update

--- app/src/main/java/com/example/firstandroid/MainActivity.kt
+++ app/src/main/java/com/example/firstandroid/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.firstandroid
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -8,8 +9,10 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.util.Log
+import com.example.firstandroid.ui.login.LoginActivity
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content2_main.*
 import java.lang.String
 
 class MainActivity : AppCompatActivity() {
@@ -19,15 +22,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        button2.text = "my_Button2"
+
+        // 因为后执行，会覆盖下面的。
         val button = findViewById<Button>(R.id.button2)
         button.setOnClickListener {
             Log.d("test", "button click")
+//            val intent = Intent(this, LoginActivity().javaClass)
+            val intent = Intent(this, TestListActivity().javaClass)
+            startActivity(intent)
         }
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+    }
+
+    fun button2OnClick(view: View) {
+        Log.d("test", "button2OnClick: $view")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
---
+++ app/src/main/java/com/example/firstandroid/ui/login/LoginViewModel.kt
@@ -0,0 +1,58 @@
+package com.example.firstandroid.ui.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import android.util.Patterns
+import com.example.firstandroid.data.LoginRepository
+import com.example.firstandroid.data.Result
+
+import com.example.firstandroid.R
+
+class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel() {
+
+    private val _loginForm = MutableLiveData<LoginFormState>()
+    val loginFormState: LiveData<LoginFormState> = _loginForm
+
+    private val _loginResult = MutableLiveData<LoginResult>()
+    val loginResult: LiveData<LoginResult> = _loginResult
+
+    fun login(username: String, password: String) {
+        // can be launched in a separate asynchronous job
+        val result = loginRepository.login(username, password)
+
+        if (result is Result.Success) {
+            _loginResult.value =
+                LoginResult(success = LoggedInUserView(displayName = result.data.displayName))
+        } else {
+            _loginResult.value = LoginResult(error = R.string.login_failed)
+        }
+    }
+
+    /**
+     * 相当于viewModel过滤一遍数据。上层输入框变化 => loginDataChanged => _loginForm => 通知上层的observer
+     */
+    fun loginDataChanged(username: String, password: String) {
+        if (!isUserNameValid(username)) {
+            _loginForm.value = LoginFormState(usernameError = R.string.invalid_username)
+        } else if (!isPasswordValid(password)) {
+            _loginForm.value = LoginFormState(passwordError = R.string.invalid_password)
+        } else {
+            _loginForm.value = LoginFormState(isDataValid = true)
+        }
+    }
+
+    // A placeholder username validation check
+    private fun isUserNameValid(username: String): Boolean {
+        return if (username.contains('@')) {
+            Patterns.EMAIL_ADDRESS.matcher(username).matches()
+        } else {
+            username.isNotBlank()
+        }
+    }
+
+    // A placeholder password validation check
+    private fun isPasswordValid(password: String): Boolean {
+        return password.length > 5
+    }
+}
